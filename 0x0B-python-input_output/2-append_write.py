31ef0750f346d8a32245c7087c56a589895c8c24
Ayodelecareer <123329621+Ayodelecareer@users.noreply.github.com>
2023-07-12 02:45:56 +0100

    Python I/O updates 001

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
+"""Defines a file-appending function."""
 
+def append_write(filename="", text=""):
+    """ function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added """
 
-def read_lines(filename="", nb_lines=0):
-    """ function that reads n lines of a text file (UTF8) and prints it
-    to stdout"""
-
-    with open(filename) as myfile:
-        lines = myfile.readlines()
-        if nb_lines <= 0 or nb_lines >= len(lines):
-            [print(i, end='') for i in lines]
-        else:
-            [print(lines[i], end='') for i in range(0, nb_lines)]
+    with open(filename, "a", encoding="utf-8") as myfile:
+        return myfile.write(text)
